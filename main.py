c9d1742e7cd2625784db34b7e88c21a663256ef8
kh <kylemhouseholder@gmail.com>
2025-05-22 09:56:00 -0500

    PASS: line funcs OK

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800,600)
 
-    l = Line(Point(120,120), Point(440,440))
-    win.draw_line(l, "white")
+    l0 = Line(Point(120,120), Point(440,440))
+    win.draw_line(l0, "white")
 
+    l1 = Line(Point(125, 125), Point(170, 450))
+    win.draw_line(l1, "green")
+
+    l2 = Line(Point(777, 222), Point(172, 454))
+    win.draw_line(l2, "blue")
 
 
 
