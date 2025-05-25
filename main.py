6409b95e365833b83aae3dbd51cb57faaa64434a
kylehouseholder <kylemhouseholder@gmail.com>
2025-05-25 18:28:59 -0500

    UPDATE: default main has easy maze with slow draw

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from field import Cell, Maze
 
 def main():
     # test
-    win = Window(1220, 1220)
-    maze = Maze(10, 10,       # starting offset
-                40, 40,       # rows, columns
-                30, 30,       # cell dimensions
-                0.000001,     # delay between steps
-                win, 216)   # seed
+    # win = Window(1220, 1220)
+    # maze = Maze(10, 10,       # starting offset
+    #             40, 40,       # rows, columns
+    #             30, 30,       # cell dimensions
+    #             0.000001,     # delay between steps
+    #             win, 216)   # seed
     # DIFFICULT
     # win = Window(1220, 1220)
     # maze = Maze(10, 10,       # starting offset
@@ -16,11 +16,14 @@ def main():
     #             30, 30,       # cell dimensions
     #             0.000001,     # delay between steps
     #             win, 2186)   # seed
-    # maze = Maze(10, 10,     # starting offset
-    #             12, 12,     # rows, columns
-    #             75, 75,     # cell dimensions
-    #             0.0035,   # delay between steps
-    #             win, 14594) # seed
+
+    # EASY
+    win = Window(920, 920)
+    maze = Maze(10, 10,     # starting offset
+                12, 12,     # rows, columns
+                75, 75,     # cell dimensions
+                0.0035,   # delay between steps
+                win, 14594) # seed
 
     maze.solve()
 
